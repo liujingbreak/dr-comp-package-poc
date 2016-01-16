bdfc8b8f3c7865c7e6b3b383ade7e27ae0478b32
Liu Jing <liujing.break@gmail.com>
2016-01-17 00:32:20 +0800

    big step of solving dependencies problem for loading node package

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,11 @@ var es = require('event-stream');
 var vp = require('vinyl-paths');
 var del = require('del');
 var jscs = require('gulp-jscs');
+var fs = require('fs');
+var log = require('log4js').getLogger('gulpfile');
+var Q = require('q');
+var mkdirs = require('mkdirs');
+var pkutil = require('./lib/packageMgr/packageUtils');
 
 var SCOPE_NAME = 'dr';
 
@@ -25,7 +30,7 @@ gulp.task('clean:dr', function() {
 
 gulp.task('clean', ['clean:dr']);
 
-gulp.task('js', function() {
+gulp.task('jscs', function() {
 	gulp.src(['*.js',
 		'lib/**/*.js'
 	]).pipe(jshint())
@@ -36,7 +41,51 @@ gulp.task('js', function() {
 	.pipe(jscs.reporter('fail'));
 });
 
+gulp.task('lp', function() {
+	return Q.all(linkPackageJson('src'));
+});
 
+function linkPackageJson(parentDir) {
+	var folders = fs.readdirSync(parentDir);
+	var qs = [];
+	folders.forEach(function(name) {
+		var dir = Path.join(parentDir, name);
+		if (fs.statSync(dir).isDirectory()) {
+			var pkJsonPath = Path.join(dir, 'package.json');
+			if (fs.existsSync(pkJsonPath)) {
+				qs.push(replicatePackageJson(pkJsonPath));
+			} else {
+				qs = qs.concat(linkPackageJson(dir));
+			}
+		}
+	});
+	return qs;
+}
+
+function replicatePackageJson(oldPath) {
+	log.debug('found ' + oldPath);
+	return Q.nfcall(fs.readFile, oldPath, {encoding: 'utf-8'})
+	.then(function(content) {
+		var json = JSON.parse(content);
+		if (json.main) {
+			var relativePath = Path.relative(
+				Path.join('node_modules', '@dr', 'someguy'),
+				Path.join(Path.dirname(oldPath), json.main));
+			log.debug('link "main" to ' + relativePath);
+			json.main = relativePath;
+		}
+
+		var packageNameObj = pkutil.parseName(json.name);
+		var newDir = Path.join('node_modules',
+			'@' + packageNameObj.scope,
+			packageNameObj.name);
+		mkdirs(newDir);
+		var newPackageJson = Path.join(newDir, 'package.json');
+		log.debug('write to ' + newPackageJson);
+		return Q.nfcall(fs.writeFile, newPackageJson,
+			JSON.stringify(json, null, '\t'));
+	});
+}
 
 gulp.task('bump-version', function() {
 	return es.merge(
