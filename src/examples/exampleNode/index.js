dadae4081ca2a919ff0907b38fcb283c791631d1
liujing <liujing@ENG0253deMacBook-Pro.local>
2016-01-18 19:10:15 +0800

    try to move express server into src folder

--- src/examples/exampleNode/index.js
+++ src/examples/exampleNode/index.js
@@ -1,5 +1,38 @@
-module.exports = function(api) {
-	console.log('Greeting from node package v%s !', require('./package.json').version);
+var log = require('log4js').getLogger('exampleNodePackage');
+var Path = require('path');
+var mafia = require('./gangsta');
+var Q = require('q');
 
-	api.route();
+module.exports = {
+	init: init,
+	haveFun: haveFun
 };
+
+function init(api) {
+	log.debug('Greeting from node package v%s !', require('./package.json').version);
+	var quote;
+
+	api.router().get('/', function(req, res) {
+		log.debug('serving GET');
+		res.render('template', {name: quote});
+	});
+
+	api.templateFolder(Path.join(__dirname, 'views'));
+
+	haveFun(api).then(function(qu) {
+		quote = qu;
+	});
+}
+
+function haveFun(api) {
+	var def = Q.defer();
+	var mafiaPromis = mafia();
+	api.eventBus.on('serverStarted', function() {
+		mafiaPromis.then(function(res) {
+			log.info('   "' + res + '"  ');
+			def.resolve(res);
+			return res;
+		});
+	});
+	return def.promise;
+}
