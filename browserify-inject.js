55e0c64075ecf7acb4b1e7317d04e8b7830e38b5
L.J <jing.liu.FE@dianrong.com>
2016-10-26 14:59:02 +0800

    Fix typo

--- browserify-inject.js
+++ browserify-inject.js
@@ -5,14 +5,14 @@ module.exports = function(injector) {
 	 *  you want by:
 	 *
 	 *  injector.fromPackage('component-package')
-	 *  	.substitue('depenency-package', 'another-package')
+	 *  	.substitute('depenency-package', 'another-package')
 	 *  	.value('depenency-package', anyValue)
 	 *  	.factory('depenency-package', function() { return anyValue; });
 	 *
 	 *  // Inject to all component packages:
 	 *
 	 *  injector.fromAllPackage()
-	 *  	.substitue('depenency-package', 'another-package');
+	 *  	.substitute('depenency-package', 'another-package');
 	 *
 	 * For example,
 	 *
