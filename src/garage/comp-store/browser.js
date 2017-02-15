a8af7feb04d50d4fa81d92ae8eb2e4361f39e245
L.J <jing.liu.FE@dianrong.com>
2017-02-15 15:11:07 +0800

    [Comp-store] Banner slogon and bug fixes

--- src/garage/comp-store/browser.js
+++ src/garage/comp-store/browser.js
@@ -21,56 +21,37 @@ exports.init = function(app) {
 			var compStoreVm = this;
 			compStoreVm.showNavi = true;
 			var scrollPanel;
-			var swiper;
+			var swiper, sc, off$viewContentLoaded;
 
 			this.$onInit = function() {
 				compStoreVm.quickSearch = drTranslate('搜索组件和小应用');
 				listPackages();
 
 				$scope.$watch('compStoreVm.nameSearch', search);
+
+				off$viewContentLoaded = $rootScope.$on('$viewContentLoaded', function(event, config) {
+					if ($state.is('components') && lastSelectedCard)
+						setTimeout(function() {
+							var scrollTarget = document.getElementById('comp-' + lastSelectedCard);
+							if (scrollTarget && scrollPanel.length > 0) {
+								var scrollTop = angular.element(scrollTarget).offset().top - scrollPanel.offset().top;
+								scrollPanel.prop('scrollTop', scrollTop);
+							}
+							initSwiper();
+							initScrollableAnim();
+						}, 17);
+					else {
+						if (scrollPanel)
+							scrollPanel.prop('scrollTop', 0);
+					}
+
+					if (!$state.is('components'))
+						destorySwiperAndAnim();
+				});
 			};
 			this.$postLink = function() {
-				//http://idangero.us/swiper/api/#.WHpddmR96qA
-				swiper = new Swiper('.swiper-container', {
-					// Optional parameters
-					//direction: 'vertical',
-					autoplay: 2500,
-					speed: 800,
-					effect: 'flip',
-					flip: {
-						slideShadows: true,
-						limitRotation: true
-					},
-					cube: {
-						slideShadows: true,
-						shadow: true,
-						shadowOffset: 20,
-						shadowScale: 0.94
-					},
-					loop: true,
-					parallax: true
-				});
-
-
-				var banner = $element.find('.banner');
-				var sc = ScrollableAnim(angular.element('body').find('.ui-view-main'), 0);
-				sc.scene({
-					begin: 0,
-					duration: banner.prop('offsetHeight'),
-					delayPercent: 0,
-					// tearDown: function(reverse, offset) {
-					// 	if (!reverse) {
-					// 		sw.stopAutoplay();
-					// 	} else
-					// 		sw.startAutoplay();
-					// },
-					onScroll: function(progress, time) {
-						var y = progress * 0.67;
-						banner.css('transform', 'translate3d(0, ' + y + 'px, 0)');
-						banner.css('-webkit-transform', 'translate3d(0, ' + y + 'px, 0)');
-						banner.css('-ms-transform', 'translate3d(0, ' + y + 'px, 0)');
-					}
-				});
+				initSwiper();
+				initScrollableAnim();
 
 				scrollPanel = $element.closest('[comp-store-scroll-pane]');
 			};
@@ -103,25 +84,66 @@ exports.init = function(app) {
 				lastSelectedCard = name;
 			};
 
-			var off$viewContentLoaded = $rootScope.$on('$viewContentLoaded', function(event, config) {
-				if ($state.is('components') && lastSelectedCard)
-					setTimeout(function() {
-						var scrollTarget = document.getElementById('comp-' + lastSelectedCard);
-						if (scrollTarget && scrollPanel.length > 0) {
-							var scrollTop = angular.element(scrollTarget).offset().top - scrollPanel.offset().top;
-							scrollPanel.prop('scrollTop', scrollTop);
-						}
-					}, 17);
-				else {
-					if (scrollPanel)
-						scrollPanel.prop('scrollTop', 0);
-				}
-			});
-
 			this.$onDestroy = function() {
 				off$viewContentLoaded();
-				swiper.destroy(true, false);
+				destorySwiperAndAnim();
 			};
+
+			function initSwiper() {
+				//http://idangero.us/swiper/api/#.WHpddmR96qA
+				swiper = new Swiper('.swiper-container', {
+					// Optional parameters
+					//direction: 'vertical',
+					autoplay: 2500,
+					speed: 800,
+					effect: 'flip',
+					flip: {
+						slideShadows: true,
+						limitRotation: true
+					},
+					cube: {
+						slideShadows: true,
+						shadow: true,
+						shadowOffset: 20,
+						shadowScale: 0.94
+					},
+					loop: true,
+					parallax: true
+				});
+			}
+
+			function initScrollableAnim() {
+				var banner = $element.find('.banner');
+				sc = ScrollableAnim(angular.element('body').find('.ui-view-main'), 0);
+				sc.scene({
+					begin: 0,
+					duration: banner.prop('offsetHeight'),
+					delayPercent: 0,
+					// tearDown: function(reverse, offset) {
+					// 	if (!reverse) {
+					// 		sw.stopAutoplay();
+					// 	} else
+					// 		sw.startAutoplay();
+					// },
+					onScroll: function(progress, time) {
+						var y = progress * 0.67;
+						banner.css('transform', 'translate3d(0, ' + y + 'px, 0)');
+						banner.css('-webkit-transform', 'translate3d(0, ' + y + 'px, 0)');
+						banner.css('-ms-transform', 'translate3d(0, ' + y + 'px, 0)');
+					}
+				});
+			}
+
+			function destorySwiperAndAnim() {
+				if (swiper) {
+					swiper.destroy(true, false);
+					swiper = null;
+				}
+				if (sc) {
+					sc.destory();
+					sc = null;
+				}
+			}
 
 			function listPackages() {
 				drLoadingService.setLoading('compStore', true);
