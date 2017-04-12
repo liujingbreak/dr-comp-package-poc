7a6c483e0412de2862892e2399515b9752a5b3bd
L.J <jing.liu.FE@dianrong.com>
2017-04-12 18:40:53 +0800

    empty Features.html

--- src/garage/doc-home/browser/js/directives/buildScenes.js
+++ src/garage/doc-home/browser/js/directives/buildScenes.js
@@ -4,11 +4,10 @@ module.exports = buildScenes;
 function buildScenes(scrollControl, iElement, scope) {
 	//var slogon = iElement.find('.screen-1').find('.center-box');
 	var scr1Parallex = angular.element('.parallex-layer.scr-1');
-	var screen2 = iElement.find('.screen-2');
-
+	var screens = iElement.find('.screen');
 	var d1 = scr1Parallex.prop('offsetHeight');
 	scrollControl.scene({
-		triggerElement: screen2,
+		triggerElement: screens.eq(1),
 		duration: d1,
 		//delayPercent: 5,
 		timeline: function(timeline) {
@@ -17,12 +16,24 @@ function buildScenes(scrollControl, iElement, scope) {
 	});
 
 	scrollControl.scene({
-		triggerElement: screen2,
+		triggerElement: screens.eq(1),
 		delayPercent: 20,
+		startup: function(reverse, offset) {
+			if (!reverse) {
+				// TODO: optimize
+				TweenMax.staggerFromTo([].slice.call(screens[1].children, 0, 2), 0.6, {autoAlpha: 0, xPercent: -50}, {autoAlpha: 1, xPercent: 0, ease: 'Power2.easeOut', paused: false}, 0.1);
+				TweenMax.staggerFromTo([].slice.call(screens[1].children, 2), 0.6, {autoAlpha: 0, xPercent: 50}, {autoAlpha: 1, xPercent: 0, ease: 'Power2.easeOut', paused: false}, 0.1);
+			}
+		}
+	});
+
+	scrollControl.scene({
+		triggerElement: screens.eq(3),
+		delayPercent: 30,
 		timeline: function(timeline) {
 			timeline.addLabel('s2');
-			timeline.to(screen2[0], 1, {backgroundColor: '#ffffff', ease: 'Power2.easeOut'});
-			timeline.fromTo(screen2[0].children[0], 1, {autoAlpha: 0}, {autoAlpha: 1, ease: 'Power2.easeOut'}, 's2');
+			timeline.to(screens[3], 1, {backgroundColor: '#ffffff', ease: 'Power2.easeOut'});
+			timeline.fromTo(screens[3].children[0], 1, {autoAlpha: 0}, {autoAlpha: 1, ease: 'Power2.easeOut'}, 's2');
 		},
 		startup: function(reverse, offset) {
 			// if (!reverse) {
@@ -32,9 +43,8 @@ function buildScenes(scrollControl, iElement, scope) {
 		}
 	});
 
-	var screen3 = iElement.find('.screen-3');
 	scrollControl.scene({
-		triggerElement: screen3,
+		triggerElement: screens.eq(screens.length - 1),
 		delayPercent: 50,
 		startup: function(reverse, offset) {
 			if (!reverse) {
