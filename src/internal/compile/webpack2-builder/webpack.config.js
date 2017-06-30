54e52ab49a464351def16f29c4656be145d8c6e4
L.J <jing.liu.FE@dianrong.com>
2017-06-30 19:33:18 +0800

    Support package.json new property "resolveModule: true"

--- src/internal/compile/webpack2-builder/webpack.config.js
+++ src/internal/compile/webpack2-builder/webpack.config.js
@@ -7,7 +7,7 @@ const log = require('log4js').getLogger(api.packageName);
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManualChunkPlugin = require('./lib/manual-chunk-plugin');
 const MultiEntryHtmlPlugin = require('./lib/multi-entry-html-plugin');
-
+const DrModuleResolvePlugin = require('./lib/dr-module-resolve-plugin');
 
 module.exports = function(webpackConfigEntry, noParse, file2EntryChunkName, entryChunkHtmlAndView,
 	legoConfig, chunk4package, sendlivereload, entryHtmlOutputPathPlugin, entryHtmlCompilePlugin) {
@@ -202,7 +202,8 @@ module.exports = function(webpackConfigEntry, noParse, file2EntryChunkName, entr
 			]
 		},
 		resolve: {
-			modules: [api.config().nodePath, 'node_modules']
+			modules: [api.config().nodePath, 'node_modules'],
+			plugins: [new DrModuleResolvePlugin()]
 		},
 		resolveLoader: {
 			modules: [__dirname, api.config().nodePath, 'node_modules']
